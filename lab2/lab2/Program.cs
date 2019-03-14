d12dd67c6f84e093400d769b6f1e954d29938f86
RuslanBrijatenkro <yahochuchalic228@gmail.com>
2019-03-15 00:00:17 +0200

    add description

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -9,18 +9,27 @@ namespace lab2
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static void Main()
 		{
-
+			int capacity = 2;
+			int processCount = 10;
+			Queue queue = new Queue(capacity);
+			ProcessFactory factory = new ProcessFactory(queue,processCount);
+			CPU cpu1 = new CPU(queue);
+			CPU cpu2 = new CPU(queue);
+			new Thread(factory.Run).Start();
+			new Thread(cpu1.Run).Start();
+			new Thread(cpu2.Run).Start();
+			Console.ReadKey();
 		}
 	}
-	class CPUProcess
+	class ProcessFactory
 	{
-		Random random = new Random();
-		CPUQueue queue;
+		
+		Queue queue;
 		int processCount;
 		int generateDelay;
-		CPUProcess(CPUQueue queue, int processCount)
+		public ProcessFactory(Queue queue, int processCount)
 		{
 			this.queue = queue;
 			this.processCount = processCount;
@@ -29,43 +38,100 @@ namespace lab2
 		{
 			for(int i=0;i<processCount;i++)
 			{
-				generateDelay = random.Next(1000, 4000);
+				generateDelay = queue.random.Next(1000, 2000);
 				Thread.Sleep(generateDelay);
-				Console.WriteLine("");
+				Console.WriteLine($"Process generated with delay {generateDelay}");
+				new Thread(()=>queue.Put("Process "+i)).Start();
 			}
+			queue.job = "done";
+			Console.WriteLine("No more processes");
 		}
 	}
 	class CPU
 	{
+		Queue queue;
+		public CPU(Queue queue)
+		{
+			this.queue = queue;
+		}
+		public void Run()
+		{
+			int processTime=queue.random.Next(2000,6000);
+			while(true)
+			{
+				try
+				{
+					if (queue.job == "done" && queue.queue.Count == 0)
+						break;
+					Console.WriteLine("CPU get process");
+					queue.Get(processTime);
+					Thread.Sleep(processTime);
+					Console.WriteLine($": {processTime}");
+				}
+				catch(Exception)
+				{
+					Console.WriteLine("Exception");
+				}
+			}
+			Console.WriteLine("Well done");
 
+		}
 	}
-	class CPUQueue
+	class Queue
 	{
+		public string job;
+		object queueLock = new object();
+		volatile bool full = false;
+		volatile bool empty = true;
+		public Random random = new Random();
 		AutoResetEvent fullQueueEvent = new AutoResetEvent(false);
-		public List<string> queue = new List<string>();
+		AutoResetEvent emptyQueueEvent = new AutoResetEvent(false);
+		public List<string> queue= new List<string>();
 		int capacity;
-		int maxSize = 0;
-		CPUQueue(int capasity)
+		public Queue(int capacity)
 		{
 			this.capacity = capacity;
 		}
-		public void Get()
+		public void Get(int processTime)
 		{
-			if(queue.Count==0)
+			if(empty)
 			{
 				Console.WriteLine("Queue is empty");
+				emptyQueueEvent.WaitOne();
 			}
-			
-		}
-		public void Put(string thread)
-		{
-			if (queue.Count == capacity)
+
+			lock (queueLock)
+				queue.RemoveAt(queue.Count-1);
+ 
+			if (queue.Count==0)
+				empty = true;
+
+			if (full)
 			{
-				Console.WriteLine($"Queue is full. Thread {thread} in waiting");
+				fullQueueEvent.Set();
+				full = false;
+			}			
+		}
+		public void Put(string process)
+		{
+			if (full)
+			{
+				Console.WriteLine($"Queue is full. Process {process} in waiting");
 				fullQueueEvent.WaitOne();
 			}
-			if (queue.Count < capacity)
-				queue.Add(thread);
+
+			lock(queueLock)
+				queue.Add(process);
+			Console.WriteLine($"{queue[queue.Count-1]} added to queue");
+
+			if (queue.Count==capacity)
+				full = true;
+
+			if (empty)
+			{
+				empty = false;
+				emptyQueueEvent.Set();
+			}
 		}
 	}
 }

9407a2b3bcb9835bcf412da87a7eca06a42d9329
RuslanBrijatenkro <yahochuchalic228@gmail.com>
2019-03-27 00:00:12 +0200

    parallel lenght,min-max Values,XOR operation

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -12,14 +12,32 @@ namespace lab2
 		static void Main()
 		{
 			int capacity = 2;
-			int processCount = 10;
+			int processCount = 30;
 			Queue queue = new Queue(capacity);
 			ProcessFactory factory = new ProcessFactory(queue,processCount);
 			CPU cpu1 = new CPU(queue);
 			CPU cpu2 = new CPU(queue);
+			CPU cpu3 = new CPU(queue);
+			CPU cpu4 = new CPU(queue);
 			new Thread(factory.Run).Start();
-			new Thread(cpu1.Run).Start();
-			new Thread(cpu2.Run).Start();
+			//for(int i=0;i<4;i++)
+			//{
+			//	Thread thread = new Thread(new CPU(queue).Run);
+			//	thread.Name = $"{i}";
+			//	thread.Start(
+			//}
+			Thread thread1 = new Thread(cpu1.Run);
+			Thread thread2=new Thread(cpu2.Run);
+			Thread thread3=new Thread(cpu3.Run);
+			Thread thread4=new Thread(cpu4.Run);
+			thread1.Name = "1";
+			thread2.Name = "2";
+			thread3.Name = "3";
+			thread4.Name = "4";
+			thread1.Start();
+			thread2.Start();
+			thread3.Start();
+			thread4.Start();
 			Console.ReadKey();
 		}
 	}
@@ -38,7 +56,7 @@ namespace lab2
 		{
 			for(int i=0;i<processCount;i++)
 			{
-				generateDelay = queue.random.Next(1000, 2000);
+				generateDelay = queue.random.Next(100, 200);
 				Thread.Sleep(generateDelay);
 				Console.WriteLine($"Process generated with delay {generateDelay}");
 				new Thread(()=>queue.Put("Process "+i)).Start();
@@ -49,32 +67,35 @@ namespace lab2
 	}
 	class CPU
 	{
+		static List<int> processesDone = new List<int>();
 		Queue queue;
 		public CPU(Queue queue)
 		{
 			this.queue = queue;
+			processesDone.Add(0);
 		}
 		public void Run()
 		{
-			int processTime=queue.random.Next(2000,6000);
+			int processTime=queue.random.Next(200,400);
 			while(true)
 			{
 				try
 				{
 					if (queue.job == "done" && queue.queue.Count == 0)
 						break;
-					Console.WriteLine("CPU get process");
 					queue.Get(processTime);
 					Thread.Sleep(processTime);
-					Console.WriteLine($": {processTime}");
+					processesDone[Convert.ToInt32(Thread.CurrentThread.Name) - 1]++;
+					Console.WriteLine($"{Thread.CurrentThread.Name}: {processTime}");
 				}
 				catch(Exception)
 				{
 					Console.WriteLine("Exception");
 				}
 			}
+			queue.emptyQueueEvent.Set();
 			Console.WriteLine("Well done");
-
+			Console.WriteLine($"{Thread.CurrentThread.Name}: "+processesDone[Convert.ToInt32(Thread.CurrentThread.Name) - 1]*100/30d+"%");
 		}
 	}
 	class Queue
@@ -85,7 +106,7 @@ namespace lab2
 		volatile bool empty = true;
 		public Random random = new Random();
 		AutoResetEvent fullQueueEvent = new AutoResetEvent(false);
-		AutoResetEvent emptyQueueEvent = new AutoResetEvent(false);
+		public AutoResetEvent emptyQueueEvent = new AutoResetEvent(false);
 		public List<string> queue= new List<string>();
 		int capacity;
 		public Queue(int capacity)
@@ -99,7 +120,7 @@ namespace lab2
 				Console.WriteLine("Queue is empty");
 				emptyQueueEvent.WaitOne();
 			}
-
+			Console.WriteLine($"CPU {Thread.CurrentThread.Name} get process");
 			lock (queueLock)
 				queue.RemoveAt(queue.Count-1);
  
---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace lab3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Algorithms algorithms = new Algorithms();
+			algorithms.Run();
+			Console.ReadKey();
+
+		}
+	}
+	class Algorithms
+	{
+		static int temporaryXOR = 0;
+		Random random = new Random();
+		static int[] intMas;
+		static long[] longMas;
+
+		static long lenght = 0;
+
+		static long min;
+		static long max;
+		static int indexMin;
+		static int indexMax;
+
+		public Algorithms()
+		{
+			intMas = new int[random.Next(5, 10)];
+			longMas = new long[random.Next(5000, 5001)];
+		}
+		public void Run()
+		{
+			for(int i=0;i<intMas.Length;i++)
+			{
+				intMas[i] = random.Next(1,5);
+			}
+			for (int i = 0; i < longMas.Length; i++)
+			{
+				longMas[i] = random.Next(100000, 100000000);
+			}
+			//XOR
+			Parallel.For(0, intMas.Length, ArrayXOR);
+			Console.WriteLine("Result: "+temporaryXOR);
+			foreach (var el in intMas)
+			{
+				Console.WriteLine(Convert.ToString(el, 2) + " ");
+			}
+			////MINMAX
+			//Parallel.For(0, longMas.Length, MinMaxValues);
+			//Console.WriteLine("Max: " + max + " Index: " + indexMax);
+			//Console.WriteLine("Min: " + min + " Index: " + indexMin);
+			//Console.WriteLine("Max: " + longMas.Max() + " Min: " + longMas.Min());
+			////LEnght
+			//Parallel.Invoke(ArrayLenght);
+			//Console.WriteLine(lenght);
+			//Console.WriteLine(intMas.Length);
+			
+		}
+		static void ArrayLenght()
+		{
+			while(true)
+			{
+				try
+				{
+					long oldValue, newValue;
+					do
+					{
+						oldValue = lenght;
+						newValue = Interlocked.Increment(ref lenght);
+						intMas[newValue]++;
+					}
+					while (oldValue != Interlocked.CompareExchange(ref lenght, newValue, oldValue));
+				}
+				catch(Exception e)
+				{
+					break;
+				}
+			}
+			Console.WriteLine("Thread end work");
+		}
+		static void ArrayXOR(int x)
+		{
+			int oldValue, newValue;
+			do
+			{
+				oldValue = temporaryXOR;
+				newValue = oldValue ^ intMas[x];
+			}
+			while (oldValue != Interlocked.CompareExchange(ref temporaryXOR, newValue, oldValue));
+		}
+		static void MinMaxValues(int x)
+		{
+			if(x==0)
+			{
+				Interlocked.Exchange(ref max, longMas[x]);
+				indexMax = x;
+				Interlocked.Exchange(ref min, longMas[x]);
+				indexMin = x;
+			}
+			if (Interlocked.Read(ref longMas[x]) > Interlocked.Read(ref max))
+			{
+				Interlocked.Exchange(ref max, longMas[x]);
+				indexMax = x;
+			}
+			if (Interlocked.Read(ref longMas[x]) < Interlocked.Read(ref min))
+			{
+				Interlocked.Exchange(ref min, longMas[x]);
+				indexMin = x;
+			}
+		}
+
+	}
+}
